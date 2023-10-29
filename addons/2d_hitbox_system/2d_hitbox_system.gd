d0233fc3ef1be8a0de6caf1d7ea670ae3fb9b425
jv-vogler <jvsvogler@gmail.com>
2023-10-29 16:32:19 -0300

    feat: update icons

--- addons/2d_hitbox_system/2d_hitbox_system.gd
+++ addons/2d_hitbox_system/2d_hitbox_system.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("Hitbox", "Area2D", preload("hitbox.gd"), preload("res://icon.svg"))
-	add_custom_type("Hurtbox", "Area2D", preload("hurtbox.gd"), preload("res://icon.svg"))
+	add_custom_type("Hitbox", "Area2D", preload("hitbox.gd"), preload("res://sword.svg"))
+	add_custom_type("Hurtbox", "Area2D", preload("hurtbox.gd"), preload("res://shield.svg"))
 
 
 func _exit_tree() -> void:
